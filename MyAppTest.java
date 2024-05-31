204e72c05a25b618e4b41f69e468c46c17faa3b0
Divyanga Lokuhetti <54659071+Divaa-07@users.noreply.github.com>
2024-05-31 16:58:57 +1000

    Create MyAppTest.java

---
+++ MyAppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class MyAppTest {
+
+    @Test
+    public void testExample() {
+        // An example test that always passes
+        assertTrue(true);
+    }
+}
